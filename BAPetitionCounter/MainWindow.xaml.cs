7a5742c2f9ccf217a59994d11efd2f9d63fd3c91
fhalfkg <bin3635@gmail.com>
2022-10-08 03:50:40 +0900

    새로고침 주기 수정 기능 추가

--- BAPetitionCounter/MainWindow.xaml.cs
+++ BAPetitionCounter/MainWindow.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
 namespace BAPetitionCounter
 {
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
     public partial class MainWindow : Window
     {
         int delay = 5000;
@@ -26,13 +30,31 @@ namespace BAPetitionCounter
                 var percentage = (double)agreed / 50000 * 100;
                 Percentage.Text = $"{Math.Truncate(percentage * 1000) / 1000}% ({string.Format("{0:n0}", agreed)}명/50,000명)";
                 _ = agreed >= 50000 ? PetitionProgressBar.Value = PetitionProgressBar.Maximum : PetitionProgressBar.Value = percentage;
-                await Task.Delay(5000);
+                Debug.WriteLine(agreed);
+                await Task.Delay(delay);
             }
         }
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        private void Exit_Click(object sender, RoutedEventArgs e)
         {
             Environment.Exit(0);
+        }
+
+        private void SetFetchingDelay_Click(object sender, RoutedEventArgs e)
+        {
+            SettingWindow settingWindow = new();
+            settingWindow.Show();
+            Debug.WriteLine(settingWindow.DelayTextBox.Text);
+        }
+
+        internal void SetDelay(int n)
+        {
+            delay = n;
+        }
+
+        internal int GetDelay()
+        {
+            return delay;
         }
     }
 }
---
+++ BAPetitionCounter/SettingWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace BAPetitionCounter
+{
+    /// <summary>
+    /// SettingWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SettingWindow : Window
+    {
+        public SettingWindow()
+        {
+            InitializeComponent();
+            Owner = Application.Current.MainWindow;
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            DelayTextBox.Text = ((MainWindow)Owner).GetDelay().ToString();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(DelayTextBox.Text, out int n))
+            {
+                if (n >= 5000)
+                {
+                    ((MainWindow)Owner).SetDelay(n);
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("값은 5000보다 커야 합니다.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("올바르지 않은 값입니다.");
+            }
+        }
+    }
+}
